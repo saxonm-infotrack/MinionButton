650c0a614c596fbce8638e36b8ba480b107474f1
saxonm-infotrack <134025508+saxonm-infotrack@users.noreply.github.com>
2023-07-27 09:25:21 +1000

    Add files via upload

--- MinionButton/MinionButton.js
+++ MinionButton/MinionButton.js
@@ -1,5 +1,5 @@
 const buttonElement = document.getElementById('Minion-Button');
-const imgElement = document.getElementById('Minion-img');
+let minionButton = document.getElementsByClassName("Minion-img")
 
 const toggleImgElement = (minionImg) => {
     if (minionImg.style.display === 'none') {
@@ -9,6 +9,15 @@ const toggleImgElement = (minionImg) => {
     }
 }
 
+const minions = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHG5TO-yM75v31thy5BH9R2QLAQfOvqdIxQzSScKrgLNrvBoOh9zdoJXbLGmntefVoYvQ&usqp=CAU",
+                 "https://static.wikia.nocookie.net/mycun-the-movie/images/f/fc/Dave_the_Minion.png/revision/latest?cb=20141115203113",
+                  "https://i.etsystatic.com/14582438/r/il/a08fac/1568698361/il_1080xN.1568698361_4dbn.jpg"]
+
+getRandomMinion = () => {
+    const randomNumber = Math.floor(Math.random() * minions.length);
+    return minions[randomNumber];
+}
+Image.src = minions
 buttonElement.addEventListener('click', () => {
-    toggleImgElement(imgElement);
+    toggleImgElement(minionButton.src = getRandomMinion());
 })
