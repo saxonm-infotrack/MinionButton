69fe08742e59ad6e8a90c3ebc2b04c3ed3ed1cf2
saxonm-infotrack <134025508+saxonm-infotrack@users.noreply.github.com>
2023-07-27 11:24:34 +1000

    Add files via upload

--- MinionButton/MinionButton.js
+++ MinionButton/MinionButton.js
@@ -1,16 +1,17 @@
 const buttonElement = document.getElementById('Minion-Button');
-const minionImg = document.getElementsByClassName("Minion-img");
+const minionImg = document.getElementById("Minion-img");
 
 
-const toggleImgElement = (minionImg) => {
-    if (minionImg.style.display === 'none') {
-        minionImg.style.display = 'block';
+const toggleImgElement = (minionImgToToggle) => {
+    if (minionImgToToggle.style.display === 'none') {
+        minionImgToToggle.style.display = 'block';
+        document.getElementById("Minion-img").src = getRandomMinion()
     } else {
-        minionImg.style.display = 'none';
+        minionImgToToggle.style.display = 'none';
     }
 }
 
-const minions = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHG5TO-yM75v31thy5BH9R2QLAQfOvqdIxQzSScKrgLNrvBoOh9zdoJXbLGmntefVoYvQ&usqp=CAU",
+ const minions = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHG5TO-yM75v31thy5BH9R2QLAQfOvqdIxQzSScKrgLNrvBoOh9zdoJXbLGmntefVoYvQ&usqp=CAU",
                  "https://static.wikia.nocookie.net/mycun-the-movie/images/f/fc/Dave_the_Minion.png/revision/latest?cb=20141115203113",
                   "https://i.etsystatic.com/14582438/r/il/a08fac/1568698361/il_1080xN.1568698361_4dbn.jpg"]
 
@@ -19,7 +20,6 @@ getRandomMinion = () => {
     return minions[randomNumber];
 }
 
-
 buttonElement.addEventListener('click', () => {
     toggleImgElement(minionImg);
-})
+}) 
