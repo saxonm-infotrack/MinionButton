404fdb6bbc76999e8bf8b8062755ea48a0044147
saxonm-infotrack <134025508+saxonm-infotrack@users.noreply.github.com>
2023-07-27 10:30:52 +1000

    Add files via upload

--- MinionButton/MinionButton.js
+++ MinionButton/MinionButton.js
@@ -5,7 +5,6 @@ const minionImg = document.getElementsByClassName("Minion-img");
 const toggleImgElement = (minionImg) => {
     if (minionImg.style.display === 'none') {
         minionImg.style.display = 'block';
-        minionImg.src = getRandomMinion()
     } else {
         minionImg.style.display = 'none';
     }
